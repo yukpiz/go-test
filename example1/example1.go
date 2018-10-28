9ddc72f4b24063c32b91cd0b2daf3d907c2f6076
yukpiz <yukpiz@gmail.com>
2018-10-28 16:01:49 +0900

    Updated test.

--- example1/example1.go
+++ example1/example1.go
@@ -1,5 +1,5 @@
 package example1
 
 func Sum(x int, y int) int {
-	return x + y
+	return x - y
 }
--- example1/example1_test.go
+++ example1/example1_test.go
@@ -13,3 +13,21 @@ func TestSum2(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestSum3(t *testing.T) {
+	if Sum(1, 2) != 3 {
+		t.Error("e", "r", "r")
+	}
+}
+
+func TestSum4(t *testing.T) {
+	if Sum(1, 2) != 3 {
+		t.Errorf("%s", "error")
+	}
+}
+
+func TestSum5(t *testing.T) {
+	if Sum(1, 2) != 3 {
+		t.FailNow()
+	}
+}
